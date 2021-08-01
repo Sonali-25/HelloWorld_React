d1cdaee3f67ff78e60f565cfe5cb2ddea2b5e3b6
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-08-01 23:40:29 +0530

    Display Using One Way Data Binding

--- hello/src/App.js
+++ hello/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      title: 'Hello Bridgelabz From Sonali Jha'
+    }
+  }
   render() {
     return (
       <div>
-        <h1> Hello From Bridgelabz</h1>
+        <h1>{this.state.title}</h1>
       </div>
     )
   }
