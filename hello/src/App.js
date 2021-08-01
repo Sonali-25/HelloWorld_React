dcee0946ece2170d6a847a84cbd3bfbfc4c01af6
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-08-02 00:51:02 +0530

    Display Hello From Bridgelabz With A User Input Name

--- hello/src/App.js
+++ hello/src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      title: 'Hello Bridgelabz From Sonali Jha'
+      userName: ''
     }
   }
 
@@ -17,13 +17,22 @@ class App extends React.Component {
     console.log("save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    this.setState({ userName: event.target.value })
+  }
   render() {
     return (
+      <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.userName} From Bridgelabz</h1>
         <img src={logo} onClick={this.onClick}/>
       </div>
-    )
+      <div>
+        <input onChange={this.onNameChange} />
+      </div>
+      </>
+    );
   }
 }
 
