e90b366e5057f5299270fa30a34a529a14021668
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-08-02 00:25:47 +0530

    Ability To Launch Bridgelabz Site In A New Tab OnClicking Bridgelabz Logo

--- hello/src/App.js
+++ hello/src/App.js
@@ -5,17 +5,23 @@ import logo from './assets/BL_Logo.png'
 
 class App extends React.Component {
 
+  url = 'https://www.bridgelabz.com/'
   constructor() {
     super()
     this.state = {
       title: 'Hello Bridgelabz From Sonali Jha'
     }
   }
+
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo}/>
+        <img src={logo} onClick={this.onClick}/>
       </div>
     )
   }
